a463fd366fe7304c5ee300b1df939d8131e25215
KarthicRavindran <karthic.ravindran@oracle.com>
2026-02-20 08:23:47 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -63,3 +63,98 @@ INTERVAL (NUMTODSINTERVAL(7,'DAY'))-- Automatically creates daily partitions
     -- Initial partition for historical data
     PARTITION p_initial VALUES LESS THAN (TIMESTAMP'2025-01-28 00:00:00 UTC')
 )
+
+###pipeline
+BEGIN
+  dbms_cloud_pipeline.create_pipeline(
+      pipeline_name =>'FINOPS_REPORT',
+      pipeline_type => 'LOAD',
+      description => 'pipeline to load FOCUS report');
+END;
+
+-- Set attributes for the dbms_cloud_pipeline.
+BEGIN
+  DBMS_CLOUD_PIPELINE.SET_ATTRIBUTE(
+    pipeline_name => 'FINOPS_REPORT',
+    attributes    => JSON_OBJECT(
+      'credential_name' VALUE 'OCI$RESOURCE_PRINCIPAL',
+      'location'        VALUE 'https://<objectstorageurl>/n/bling/b/<tenancyocid>/o/FOCUS Reports/<year>',
+      'table_name'      VALUE 'OCI_FOCUS_REPORTS',
+      'field_list'   VALUE q'[
+     "AVAILABILITYZONE"            CHAR(4000)
+    ,"BILLEDCOST"                  CHAR
+    ,"BILLINGACCOUNTID"            CHAR
+    ,"BILLINGACCOUNTNAME"          CHAR(32767)
+    ,"BILLINGCURRENCY"             CHAR(4000)
+    ,"BILLINGPERIODEND"            CHAR date_format TIMESTAMP WITH TIME ZONE MASK "YYYY-MM-DD\"T\"HH24:MI:SS.FF9TZR"
+    ,"BILLINGPERIODSTART"          CHAR date_format TIMESTAMP WITH TIME ZONE MASK "YYYY-MM-DD\"T\"HH24:MI:SS.FF9TZR"
+    ,"CHARGECATEGORY"              CHAR(4000)
+    ,"CHARGEDESCRIPTION"           CHAR(4000)
+    ,"CHARGEFREQUENCY"             CHAR(4000)
+    ,"CHARGEPERIODEND"             CHAR date_format TIMESTAMP WITH TIME ZONE MASK "YYYY-MM-DD\"T\"HH24:MITZH:TZM"
+    ,"CHARGEPERIODSTART"           CHAR date_format TIMESTAMP WITH TIME ZONE MASK "YYYY-MM-DD\"T\"HH24:MITZH:TZM"
+    ,"CHARGESUBCATEGORY"           CHAR(32767)
+    ,"COMMITMENTDISCOUNTCATEGORY"  CHAR(32767)
+    ,"COMMITMENTDISCOUNTID"        CHAR(32767)
+    ,"COMMITMENTDISCOUNTNAME"      CHAR(32767)
+    ,"COMMITMENTDISCOUNTTYPE"      CHAR(32767)
+    ,"EFFECTIVECOST"               CHAR
+    ,"INVOICEISSUER"               CHAR(4000)
+    ,"LISTCOST"                    CHAR
+    ,"LISTUNITPRICE"               CHAR
+    ,"PRICINGCATEGORY"             CHAR(32767)
+    ,"PRICINGQUANTITY"             CHAR
+    ,"PRICINGUNIT"                 CHAR(4000)
+    ,"PROVIDER"                    CHAR(4000)
+    ,"PUBLISHER"                   CHAR(4000)
+    ,"REGION"                      CHAR(4000)
+    ,"RESOURCEID"                  CHAR(4000)
+    ,"RESOURCENAME"                CHAR(32767)
+    ,"RESOURCETYPE"                CHAR(4000)
+    ,"SERVICECATEGORY"             CHAR(4000)
+    ,"SERVICENAME"                 CHAR(4000)
+    ,"SKUID"                       CHAR(4000)
+    ,"SKUPRICEID"                  CHAR(32767)
+    ,"SUBACCOUNTID"                CHAR(4000)
+    ,"SUBACCOUNTNAME"              CHAR(4000)
+    ,"TAGS"                        CHAR(32767)
+    ,"USAGEQUANTITY"               CHAR
+    ,"USAGEUNIT"                   CHAR(4000)
+    ,"OCI_REFERENCENUMBER"         CHAR(4000)
+    ,"OCI_COMPARTMENTID"           CHAR(4000)
+    ,"OCI_COMPARTMENTNAME"         CHAR(4000)
+    ,"OCI_OVERAGEFLAG"             CHAR(4000)
+    ,"OCI_UNITPRICEOVERAGE"        CHAR
+    ,"OCI_BILLEDQUANTITYOVERAGE"   CHAR
+    ,"OCI_COSTOVERAGE"             CHAR
+    ,"OCI_ATTRIBUTEDUSAGE"         CHAR
+    ,"OCI_ATTRIBUTEDCOST"          CHAR
+    ,"OCI_BACKREFERENCENUMBER" CHAR]',
+    'format'      VALUE '{
+         "type": "csv",
+         "delimiter" : ",",
+         "ignoremissingcolumns" : true,
+         "ignoreblanklines" : true,
+         "blankasnull" : true,
+         "trimspaces" : "lrtrim",
+         "quote" : "\"",
+         "characterset" : "AL32UTF8",
+         "skipheaders" : 1,
+         "logprefix" : "FOCUS",
+         "logretention" : 7,
+         "rejectlimit" : 10000000,
+         "recorddelimiter" : "X''0D0A''",
+         "compression": "gzip"
+         }',
+      'priority' VALUE 'HIGH',
+      'interval' VALUE '240'
+    )
+  );
+END;
+
+--start the pipeline
+begin
+  dbms_cloud_pipeline.start_pipeline(
+    pipeline_name => 'FINOPS_REPORT'
+  );
+end;
